797974ec422253502f797ceebcbcc7a066d7d6e6
bestedudevelopment <admin.svtbest@gmail.com>
2026-08-22 17:52:47 +0530

    Update firebase.js

--- core/firebase.js
+++ core/firebase.js
@@ -1,27 +1,50 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
- export const firebaseConfig = {
-    apiKey: "AIzaSyCvpd7HxpLq4IJ22cMTldV3sanz35tt3H8",
-    authDomain: "best----bus-application.firebaseapp.com",
-    projectId: "best----bus-application",
-    storageBucket: "best----bus-application.firebasestorage.app",
-    messagingSenderId: "428353631166",
-    appId: "1:428353631166:web:e58e03cb28a52a790e3fef",
-    measurementId: "G-5XP2LE6FH1"
+import {
+    getAuth,
+    setPersistence,
+    browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCvpd7HxpLq4IJ22cMTldV3sanz35tt3H8",
+  authDomain: "best----bus-application.firebaseapp.com",
+  projectId: "best----bus-application",
+  storageBucket: "best----bus-application.firebasestorage.app",
+  messagingSenderId: "428353631166",
+  appId: "1:428353631166:web:e58e03cb28a52a790e3fef",
+  measurementId: "G-5XP2LE6FH1"
 };
 
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
 
+// Firebase Authentication
+const auth = getAuth(app);
+
+
+// Keep users logged in
+setPersistence(
+    auth,
+    browserLocalPersistence
+);
+
+
+// Cloud Firestore
+const db = getFirestore(app);
+
+
+// Export
 export {
     app,
     auth,
-    db,
-    storage
+    db
 };
